a0640d997e21a46b94ad48f401c3b089f489aaac
Caleb St-Denis <caleb.stdenis@gmail.com>
2017-07-17 09:58:15 -0400

    improved search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import RaisedButton from 'material-ui/RaisedButton';
-=======
 import AutoComplete from 'material-ui/AutoComplete';
 import {MenuItem} from 'material-ui/Menu';
+import TextField from 'material-ui/TextField';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import SearchIcon from 'material-ui/svg-icons/action/search';
 import semesterIcons from '../SemesterIcons';
 import fp from 'lodash/fp';
-
 
 const coursesToSearchResults = (courses) => {
     return courses.map(course => {
@@ -35,7 +34,8 @@ const coursesToSearchResults = (courses) => {
         }
     });
 }
->>>>>>> 1e9d3b9... Basic working search
+
+const courseToString = (course) => `${course.code} - ${course.title}`;
 
 export default class SearchBar extends React.Component {
 
@@ -76,6 +76,52 @@ export default class SearchBar extends React.Component {
         }
     }
 
+    onSearchTextUpdate = (event, newText) => {
+        const filteredCourses = this.state.courses.filter(course => courseToString(course).toLowerCase().includes(newText.toLowerCase()));
+        this.setState({
+            searchText: newText,
+            searchActive: true,
+            searchResults: filteredCourses,
+            selectedCourse: undefined
+        });
+    }
+
+    courseSelected = (course) => {
+        this.setState({
+            searchText: courseToString(course),
+            selectedCourse: course
+        })
+    }
+
+    coursesToSearchResults = (courses) => {
+        if(!courses) {
+            return (<div>No courses</div>);
+        }
+        let listItem = courses.map( (course, index) => (
+            <div>
+                <ListItem onTouchTap={() => this.courseSelected(course)}>
+                    <div style={{display: "flex", justifyContent: "space-between"}}>
+                        <div>{`${course.code} - ${course.title}`}</div>
+                        <div>
+                            {
+                                fp.flow(
+                                    fp.map(section => ({term: section.term, year: section.year})),
+                                    fp.uniqWith(fp.isEqual),
+                                    fp.map(section => <span title={`This course is offered in ${fp.capitalize(section.term)} ${section.year}.`}>{semesterIcons[section.term]}</span>)
+                                )(course.sections)
+                            }
+                        </div>
+                    </div>
+                </ListItem>
+                {index == courses.length-1 ? null : <Divider/>}
+            </div>
+        ));
+
+        return (
+            <List>{listItem}</List>
+        );
+    }
+
     render() {
         const styles = {
             searchBarContainer: {
@@ -90,19 +136,23 @@ export default class SearchBar extends React.Component {
             }
         }
         return (
-            <Paper 
-                zDepth={1}
-                style={styles.searchBarContainer}
-            >
-                <SearchIcon style={styles.leftIcon}/>
-                <AutoComplete
-                    filter={AutoComplete.caseInsensitiveFilter}
-                    fullWidth={true}
-                    hintText="Search for a course"
-                    dataSource={coursesToSearchResults(this.state.courses)}
-                    underlineStyle={{display: 'none'}}
-                />
-            </Paper>
+            <div>
+                <Paper 
+                    zDepth={1}
+                    style={styles.searchBarContainer}
+                >
+                    <SearchIcon style={styles.leftIcon}/>
+                    <TextField
+                        fullWidth={true}
+                        hintText="Search for a course"
+                        underlineShow={false}
+                        onFocus={() => this.onSearchTextUpdate({}, this.state.searchText || "")}
+                        onChange={this.onSearchTextUpdate}
+                        value={this.state.searchText}
+                    />
+                </Paper>
+                {this.coursesToSearchResults(this.state.searchResults)}
+            </div>
         );
     }
 }
