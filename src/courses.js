3f45be0aebf28220d965ebc320d4950a51a2de81
karun <karunmalik33@gmail.com>
2017-07-17 23:03:49 -0400

    added courses

--- src/courses.js
+++ src/courses.js
@@ -84,9 +84,269 @@ const courses = [
                 year: "2017",
                 prof: {
                     name: "Nejib Zaguia",
-                    id: "42416"
+                    rating: "42416"
+                },
+                letter: "A",
+                lectures: [
+                {
+                    day: "Monday",
+                    time: "16:00 - 17:30"
+                },
+                {
+                    day: "Friday",
+                    time: "14:30 - 16:00"
                 }
+                ],
+            },
+            {
+                term: "fall",
+                year: "2017",
+                prof: {
+                    name: "Jean-Lou De Carufel",
+                    rating: "1524082"
+                },
+                 letter: "B",
+                  lectures: [
+                {
+                    day: "Monday",
+                    time: "10:00 - 11:30"
+                },
+                {
+                    day: "Wednesday",
+                    time: "13:00 -14:30"
+                }
+                ],
             }
+        ]
+    },
+    {
+        title: "Introduction to Data Communications and Networking",
+        code: "CEG3185",
+        sections: [
+            {
+                term: "summer",
+                year: "2017",
+                prof: {
+                    name: "Melike Erol Kantarci",
+                    rating: "2009037"
+                },
+                letter: "A",
+                lectures: [
+                    {
+                        day: "Monday",
+                        time: "11:30-13:00"
+                    },
+                    {
+                        day: "Thursday",
+                        time: "13:00-14:30"
+                    }
+                ],
+                labs: [
+                    {
+                        day: "Tuesday",
+                        time: "13:00-14:30"
+                    },
+                    {
+                        day: "Thursday",
+                        time: "11:30-13:00"
+                    },
+                    {
+                        day: "Friday",
+                        time: "11:30-13:00"
+                    }
+                ],
+                dgd: [
+                    {
+                        day: "Wednesday",
+                        time: "13:00-14:30"
+                    }
+                ],
+            },
+            {
+                term: "winter",
+                year: "2018",
+                prof: {
+                    name: "Jiying Zhao",
+                    rating: "39197"
+                },
+                letter: "B",
+                lectures: [
+                    {
+                        day: "Tuesday",
+                        time: "10:00-11:30"
+                    },
+                    {
+                        day: "Thursday",
+                        time: "8:30-10:00"
+                    }
+                ],
+                labs: [
+                    {
+                        day: "Tuesday",
+                        time: "14:30-16:00"
+                    },
+                    {
+                        day: "Wednesday",
+                        time: "8:30-10:00"
+                    },
+                    {
+                        day: "Wednesday",
+                        time: "10:00-11:30"
+                    }
+                ],
+                dgd: [
+                    {
+                        day: "Friday",
+                        time: "14:30-16:00"
+                    }
+                ],
+            }
+        ]
+    },
+    {
+        title: "Operating Systems",
+        code: "CSI3131",
+        sections: [
+            {
+                term: "summer",
+                year: "2017",
+                prof: {
+                    name: "Sherif Aly Ahmed",
+                    rating: "1979950"
+                },
+                letter: "A",
+                lectures: [
+                    {
+                        day: "Monday",
+                        time: "13:00-16:00"
+                    }                 
+                ],
+                labs: [
+                    {
+                        day: "Wednesday",
+                        time: "17:30-19:00"
+                    },
+                    {
+                        day: "Friday",
+                        time: "11:30-13:00"
+                    }
+                ],
+                dgd: [
+                    {
+                        day: "Friday",
+                        time: "14:30-16:00"
+                    }
+                ],
+            },
+            {
+                term: "winter",
+                year: "2018",
+                prof: {
+                    name: "Amiya Nayak",
+                    rating: "301265"
+                },
+                letter: "B",
+                lectures: [
+                    {
+                        day: "Thursday",
+                        time: "14:00-16:30"
+                    }
+                ],
+                labs: [
+                    {
+                        day: "Tuesday",
+                        time: "17:30-19:00"
+                    },
+                    {
+                        day: "Tuesday",
+                        time: "17:30-19:00"
+                    }
+                ],
+                dgd: [
+                    {
+                        day: "Tuesday",
+                        time: "16:00-17:30"
+                    }
+                ],
+            }
+        ]
+    },
+    {
+        title: "Software Quality Assurance",
+        code: "SEG3103",
+        sections: [
+            {
+                term: "summer",
+                year: "2017",
+                prof: {
+                    name: "Sherif Aly Ahmed",
+                    rating: "1979950"
+                },
+                letter: "A",
+                lectures: [
+                    {
+                        day: "Friday",
+                        time: "8:30-11:30"
+                    }                 
+                ],
+                labs: [
+                    {
+                        day: "Friday",
+                        time: "16:00-17:30"
+                    },
+                    {
+                        day: "Friday",
+                        time: "16:00-17:30"
+                    }
+                ],
+                dgd: [
+                    {
+                        day: "Friday",
+                        time: "14:00-16:30"
+                    }
+                ],
+            }        
+        ]
+    },
+    {
+        title: "Software Requirements Analysis",
+        code: "SEG3101",
+        sections: [
+            {
+                term: "fall",
+                year: "2017",
+                prof: {
+                    name: "Daniel Amyot",
+                    rating: "106789"
+                },
+                letter: "A",
+                lectures: [
+                    {
+                        day: "Tuesday",
+                        time: "14:30-16:00"
+                    },
+                    {
+                        day: "Friday",
+                        time: "16:30-17:30"
+                    }                
+                ],
+                labs: [
+                    {
+                        day: "Friday",
+                        time: "13:00-14:30"
+                    },
+                    {
+                        day: "Friday",
+                        time: "13:00-14:30"
+                    }
+                ],
+                dgd: [
+                    {
+                        day: "Friday",
+                        time: "17:30-19:00"
+                    }
+                ],
+            }        
         ]
     }
 ]
