bec3ce44aebc68a88c02603b77a36547682f6b42
Aakash Sethi <aakashsethi20@gmail.com>
2017-07-19 19:24:02 -0400

    added CourseFinder

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import SimpleLookup from './components/SimpleLookup';
 import AppBar from './components/AppBar';
 import {teal500} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import CourseFinder from './components/CourseFinder';
 
 const muiTheme = getMuiTheme({
   palette: {
     primary1Color: teal500
   }
 });
+
 
 class App extends React.Component {
 
