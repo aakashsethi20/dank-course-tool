4f46f2ebec1dba54cf2f8a5548b51f21b8b87731
Caleb St-Denis <caleb.stdenis@gmail.com>
2017-07-19 11:11:16 -0400

    finished with course section finally

--- src/components/CourseSection.js
+++ src/components/CourseSection.js
@@ -8,7 +8,7 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
-import Divider from 'material-ui/Divider';
+import './CourseSection.css'
 
 export default class CourseSection extends React.Component {
     constructor(props) {
@@ -16,87 +16,56 @@ export default class CourseSection extends React.Component {
     }
 
     render () {
-
-        const style = {
-           
-        };
-
-        let lectures = this.props.section.lectures;
-        const lectureElems = lectures.map(lecture => (
-            <TableRow>
-                <TableHeaderColumn>Lecture</TableHeaderColumn>
-                <TableRowColumn>{lecture.day}}</TableRowColumn>
-                <TableRowColumn>{lecture.time}</TableRowColumn>
-            </TableRow>
-        ));
-
-        let labs = this.props.section.labs;
-        const labElems = labs.map(lab => (
-            <div>{lab.day}&emsp;{lab.time}</div>
-        ));
-
-        let dgds = this.props.section.dgd;
-        const dgdElems = dgds.map(dgd => (
-            <div>{dgd.day}&emsp;{dgd.time}</div>
-        ));
-
         const styles = {
             container: {
                 margin: "1em auto",
                 width:"auto"
-            },
-            table: {
-                borderCollapse: "collapse",
-                width: "100%"
-            },
-            row2: {
-                height: "28px",
-                borderBottom: "1px solid rgb(224, 224, 224)"
-            },
-            row1: {
-                height: "28px"
-            },
-            row: {
-                height: "48px",
-                borderBottom: "1px solid rgb(224, 224, 224)"
-            },
-            lastRow: {
-                color: "rgba(0, 0, 0, 0.87)", 
-                height: "48px",
             }
         }
 
-        // let getActivity = activity => {
-
-        // });
+        const activities = {
+            lectures: "Lectures",
+            labs: "Labs",
+            dgd: "Tutorials"
+        }
+        
 
         return (
             <Paper style={styles.container} zDepth={2}>
-                <table style={styles.table}>
-                    <tr style={styles.row}>
-                        <th style={{width: "8em"}}>Section</th>
-                        <td style={{width: "3em"}}>A</td>
-                        <th style={{width: "6em", textAlign: "left", paddingLeft: "1em"}}>Professor</th>
-                        <td style={{width: "15em"}}>Fadi Malek</td>
-                    </tr>
-                    <tr style={styles.row}>
-                        <th rowSpan={1}>Lectures</th>
-                        <td></td>
-                        <td style={{paddingLeft: "1em"}}>Thursday</td>
-                        <td>10:00-11:30</td>
-                    </tr>
-                    {/* <tr style={styles.row1}>
-                        <th rowSpan={2}>Lectures</th>
-                        <td></td>
-                        <td>Thursday</td>
-                        <td>10:00-11:30</td>
-                    </tr>
-                    <tr style={styles.row2}>
-                        <td></td>
-                        <td>Thursday</td>
-                        <td>10:00-11:30</td>
-                    </tr> */}
-                </table>
+                <Table selectable={false}>
+                    <TableBody displayRowCheckbox ={false}>
+                    <TableRow style={styles.row}>
+                        <TableRowColumn style={{paddingRight: "0"}}>
+                            <strong>Section</strong><span style={{marginLeft: "3vw"}}>A</span>
+                        </TableRowColumn>
+                        <TableRowColumn style={{paddingRight: "0"}}>
+                            <strong>Professor</strong>
+                        </TableRowColumn>
+                        <TableRowColumn style={{width: "35%", paddingLeft: "0"}}>
+                            <a 
+                                    target="_blank" 
+                                    href={`http://www.ratemyprofessor.com/ShowRatings.jsp?tid=${this.props.section.prof.rating}`}
+                                    style={{}}
+                            >{this.props.section.prof.name}</a>
+                        </TableRowColumn>
+                    </TableRow>
+                    {
+                        ["lectures", "labs", "dgd"].map(activity => (
+                            !this.props.section[activity] ? null :
+                                this.props.section[activity].map((dateTime,index,array) => (
+                                    <TableRow displayBorder={index === array.length - 1}>
+                                        {
+                                            index !== 0 ? null : <TableRowColumn rowSpan={array.length}><strong>{activities[activity]}</strong></TableRowColumn>
+                                        }
+                                        <TableRowColumn style={{paddingRight: "0"}}>{dateTime.day}</TableRowColumn>
+                                        <TableRowColumn style={{paddingLeft: "0"}}>{dateTime.time}</TableRowColumn>
+                                    </TableRow>
+                                ))
+                            )
+                        )
+                    }
+                    </TableBody>
+                </Table>
             </Paper>
         );
     }
