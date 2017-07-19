057048e47218959fad98e520d445e664bdc50bcd
Caleb St-Denis <caleb.stdenis@gmail.com>
2017-07-19 16:31:28 -0400

    details transition

--- src/components/SimpleLookup.js
+++ src/components/SimpleLookup.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import {Card, CardHeader} from 'material-ui/Card';
 import CourseDetails from './CourseDetails';
+import { Transition } from 'react-transition-group';
+import './SimpleLookup.css'
 
 export default class SimpleLookup extends React.Component {
 
@@ -20,10 +22,33 @@ export default class SimpleLookup extends React.Component {
     }
 
     render() {
+    
+        const duration = 350;
+        const defaultStyle = {
+            transition: `opacity ${duration}ms ease-in-out`,
+            opacity: 0,
+        }
+
+        const transitionStyles = {
+            entering: { opacity: 1 },
+            entered:  { opacity: 1 },
+        };
+
         return (
             <div>
                 <SearchBar onCourseSelected={this.onCourseSelected}/>
-                {this.state.selectedCourse ? <CourseDetails course={this.state.selectedCourse}/> : null}
+                <Transition in={!!this.state.selectedCourse} timeout={duration}>
+                    {(state) => (
+                    <div 
+                    course={this.state.selectedCourse}
+                    style={{
+                            ...defaultStyle,
+                            ...transitionStyles[state]
+                        }}>
+                            {this.state.selectedCourse ? <CourseDetails course={this.state.selectedCourse}/> : null}
+                    </div>
+    )}
+                </Transition>
             </div>
         );
     }
